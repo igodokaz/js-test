88407213969c2e52378a6cea8f16fc458486f86f
JEONG JINBO <igodokaz@gmail.com>
2021-10-26 16:10:35 +0900

    JS 강의 3

--- main.js
+++ main.js
@@ -10,8 +10,43 @@
 
 // const a = 2;
 
-let a = 2;
-// a = a + 1
-a += 1;
+// let a = 2;
+// // a = a + 1
+// a += 1;
+
+// console.log(a);
+
+// 비교 연산자(comparison operate)
+
+// const a = 1;
+// const b = 3;
+
+// console.log(a === b);
+
+// function isEqual(x, y) {
+//   return x === y;
+// }
+
+// console.log(isEqual(1, 1));
+// console.log(isEqual(2, "2"));
+
+// const a = 13;
+// const b = 13;
+
+// // console.log(a !== b);
+// console.log(a >= b);
+
+// 논리 연산자(logical operator)
+
+const a = 1 === 1;
+const b = "AB" === "AB";
+const c = false;
 
 console.log(a);
+console.log(b);
+console.log(c);
+
+console.log("&&: ", a && b && c); // 그리고, and
+console.log("||:", a || b || c); // 또는, or
+
+console.log("!:", !a); // 부정, not
