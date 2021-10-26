1a9f3b1a7d4a50566f03deb20b5389a4fb3b89b3
JEONG JINBO <igodokaz@gmail.com>
2021-10-26 15:42:58 +0900

    JS 강의 1

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import getType from "./getType.js";
+
 console.log(typeof "hello world");
 console.log(typeof 123);
 console.log(typeof true);
@@ -5,3 +7,9 @@ console.log(typeof undefined);
 console.log(typeof null);
 console.log(typeof {});
 console.log(typeof []);
+
+console.log(getType(123));
+console.log(getType(false));
+console.log(getType(null));
+console.log(getType({}));
+console.log(getType([]));
