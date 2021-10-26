196566a51cafb62caf5b33de2f703c55c1aa7078
JEONG JINBO <igodokaz@gmail.com>
2021-10-26 16:35:11 +0900

    JS 6강

--- main.js
+++ main.js
@@ -69,12 +69,26 @@ import random from "./getRandom.js";
 
 const a = random();
 
-if (a === 0) {
-  console.log("a is 0");
-} else if (a === 2) {
-  console.log("a is 2");
-} else if (a === 4) {
-  console.log("a is 4");
-} else {
-  console.log("rest...");
+switch (a) {
+  case 0:
+    console.log("a is 0");
+    break;
+  case 2:
+    coxnsole.log("a is 2");
+    break;
+  case 4:
+    console.log("a is 4");
+    break;
+  default:
+    console.log("rest...");
 }
+
+// if (a === 0) {
+//   console.log("a is 0");
+// } else if (a === 2) {
+//   console.log("a is 2");
+// } else if (a === 4) {
+//   console.log("a is 4");
+// } else {
+//   console.log("rest...");
+// }
