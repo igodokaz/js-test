fcaa381513b9716afbcc7403d35790f3a21e76fa
JEONG JINBO <igodokaz@gmail.com>
2021-10-26 16:56:16 +0900

    JS 7강

--- main.js
+++ main.js
@@ -65,25 +65,25 @@
 
 // 조건문 (IF statement) //
 
-import random from "./getRandom.js";
+// import random from "./getRandom.js";
 
-const a = random();
+// const a = random();
 
-switch (a) {
-  case 0:
-    console.log("a is 0");
-    break;
-  case 2:
-    coxnsole.log("a is 2");
-    break;
-  case 4:
-    console.log("a is 4");
-    break;
-  default:
-    console.log("rest...");
-}
+// switch (a) {
+//   case 0:
+//     console.log("a is 0");
+//     break;
+//   case 2:
+//     coxnsole.log("a is 2");
+//     break;
+//   case 4:
+//     console.log("a is 4");
+//     break;
+//   default:
+//     console.log("rest...");
+// }
 
-// if (a === 0) {
+// // if (a === 0) {
 //   console.log("a is 0");
 // } else if (a === 2) {
 //   console.log("a is 2");
@@ -92,3 +92,19 @@ switch (a) {
 // } else {
 //   console.log("rest...");
 // }
+
+// 반복문 (for statenent) ????????
+// for (시작조건; 종료조건; 변화조건) {}
+
+const ulEl = document.querySelector("ul");
+
+for (let i = 0; i < 10; i += 1) {
+  const li = document.createElement("li");
+  li.textContent = "list-${i + 1}";
+  if ((i + 1) % 2 === 0) {
+    li.addEventListener("click", function () {
+      console.log(li.textContent);
+    });
+  }
+  ulEl.appendChild(li);
+}
