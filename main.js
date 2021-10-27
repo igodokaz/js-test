e40d8031f24dcddbd0d882e5db3811c8b0706e4c
JEONG JINBO <igodokaz@gmail.com>
2021-10-27 14:15:37 +0900

    ch.3 JS 5강

--- main.js
+++ main.js
@@ -94,3 +94,31 @@
 // function double() {
 //   console.log(a * 2);
 // }
+
+// 타이머 함수
+// setTimeout(함수, 시간): 일정 시간 후 함수 실행
+// setInterval(함수, 시간): 시간 간격마다 함수 실행
+// clearTimeout(): 설정된 Timeout 함수를 종료
+// clearInterval(): 설정된 Interval 함수를 종료
+
+// setTimeout(function () {
+//   console.log("heropy!");
+// }, 3000); //3초 (ms)
+
+// const timer = setTimeout(() => {
+//   console.log("Heropy!");
+// }, 3000);
+
+// const h1El = document.querySelector("h1");
+// h1El.addEventListener("click", () => {
+//   clearTimeout(timer);
+// });
+
+const timer = setInterval(() => {
+  console.log("Heropy!");
+}, 3000);
+
+const h1El = document.querySelector("h1");
+h1El.addEventListener("click", () => {
+  clearInterval(timer);
+});
