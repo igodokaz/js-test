c0d48280afeb595aa2ccc4292ffd8690911c1cec
JEONG JINBO <igodokaz@gmail.com>
2021-10-27 15:18:38 +0900

    ch.4 JS 강의1

--- main.js
+++ main.js
@@ -1,139 +1,48 @@
-// 함수 복습
+// class 설명을 위한 사전내용
 
-// function sum(x, y) {
-//   console.log(x + y);
-// }
-
-// sum(1, 3);
-// sum(4, 12);
-
-// 기명함수
-// function sum(x, y) {
-//   return x + y;
-// }
-
-// const a = sum(1, 3);
-// const b = sum(4, 12);
-
-// console.log(a);
-// console.log(a + b);
-// console.log(sum(1, 3)); //단일로 사용될 때 좋음
-// console.log(sum(1, 3) + b); //비효율적
-
-// // 익명함수
-// function sum(x, y) {
-//   return x + y; // 함수가 종료됨을 의미, 밑의 내용은 실행안됨
-//   console.log(x);
-// }
-
-// sum(1, 3);
-
-// function sum(x, y) {
-//   if (x < 2) {
-//     return; // undefined, 값이 정해지지 않고 종료
-//     return 123; // 출력 x
-//   }
-//   return x + y;
-// }
-// console.log(sum(1, 3));
-
-// function sum() {
-//   console.log(arguments);
-//   return arguments[0] + arguments[1]; // 인수가 많은 경우 활용, 일괄적 활용, 많이 사용 안됨
-// }
-
-// console.log(sum(7, 3));
-
-// 화살표 함수
-// () => {} vs function () {}
-
-// const double = function (x) {
-//   return x * 2;
+// const heropy = {
+//   firstName: "Heropy",
+//   lastName: "Park",
+//   getFullName: function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
 // };
-// console.log("double: ", double(7));
+// console.log(heropy.getFullName());
 
-// const doubleArrow = (x) => {
-//   return x * 2;
+// const amy = {
+//   firstName: "Amy",
+//   lastName: "Clarke",
+//   getFullName: function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
 // };
-// console.log("doubleArrow", doubleArrow(7));
+// console.log(amy.getFullName());
 
-// const doubleArrow = (x) => null;
-// console.log("doubleArrow: ", doubleArrow(7));
-
-// const doubleArrow = (x) => ({ name: "Heropy" }); // 문자, 숫자, true/f, null, undefined, []배열데이터도 됨
-// console.log("doubleArrow", doubleArrow(7));
-
-// 즉시실행함수
-// IIFE, Immediately-Invoked Function Expression
-
-// const a = 7;
-// function double() {
-//   console.log(a * 2);
-// }
-// double();
-
-// (function () {
-//   console.log(a * 2);
-// })(); // ()) 도 가능, prettier가 자동 변환
-
-// 호이스팅 (Hoisting)
-// 함수 선언부가 유효범위 최상단으로 끌어올려지는 현상
-
-// 실행불가
-// const a = 7;
-// double();
-
-// const double = function () {
-//   console.log(a * 2);
+// const neo = {
+//   firstName: "Neo",
+//   lastName: "Smith",
+//   getFullName: function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
 // };
+// console.log(neo.getFullName());
 
-//실행 가능
-// const a = 7;
-// double();
+// class 설명
 
-// function double() {
-//   console.log(a * 2);
-// }
-
-// 타이머 함수
-// setTimeout(함수, 시간): 일정 시간 후 함수 실행
-// setInterval(함수, 시간): 시간 간격마다 함수 실행
-// clearTimeout(): 설정된 Timeout 함수를 종료
-// clearInterval(): 설정된 Interval 함수를 종료
-
-// setTimeout(function () {
-//   console.log("heropy!");
-// }, 3000); //3초 (ms)
-
-// const timer = setTimeout(() => {
-//   console.log("Heropy!");
-// }, 3000);
-
-// const h1El = document.querySelector("h1");
-// h1El.addEventListener("click", () => {
-//   clearTimeout(timer);
-// });
-
-// const timer = setInterval(() => {
-//   console.log("Heropy!");
-// }, 3000);
-
-// const h1El = document.querySelector("h1");
-// h1El.addEventListener("click", () => {
-//   clearInterval(timer);
-// });
-
-// 콜백(Callback)
-// 함수의 인수로 사용되는 함수
-
-// setTimeout(함수, 시간)
-
-function timeout(cb) {
-  setTimeout(() => {
-    console.log("Heropy!");
-    cb();
-  }, 3000);
+function User(first, last) {
+  // 파스칼 케이스? U를 대문자로
+  this.firstName = first;
+  this.lastName = last;
 }
-timeout(() => {
-  console.log("Done!");
-});
+
+User.prototype.getFullName = function () {
+  return `${this.firstName} ${this.lastName}`;
+};
+
+const heropy = new User("Heropy", "Part"); // 생성자함수
+const amy = new User("Amy", "Clarke");
+const neo = new User("Neo", "Smith");
+
+console.log(heropy.getFullName());
+console.log(amy.getFullName());
+console.log(neo);
